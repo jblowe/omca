ae9fefb4c4305f8aad51d197251f85fd5c7c434c
John Lowe <jblowe@berkeley.edu>
2016-05-04 02:41:44 -0700

    code cleanup for etl script

--- etl/solrETL-public.sh
+++ etl/solrETL-public.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 date
 ##############################################################################
-# move the current set of extracts to temp (thereby saving the previous run, just in case)
+# copy the current set of extracts to temp (thereby saving the previous run, just in case)
 ##############################################################################
 cp 4solr.*.csv.gz /tmp
 ##############################################################################
@@ -10,8 +10,7 @@ cp 4solr.*.csv.gz /tmp
 # eases maintainance. ergo, the TENANT parameter
 ##############################################################################
 TENANT=$1
-# host=localhost dbname=omca_domain_omca user=nuxeo_omca password=nuxeo sslmode=prefer
-SERVER="localhost sslmode=prefer password=nuxeo"
+SERVER="localhost sslmode=prefer password=xxxx"
 USERNAME="nuxeo_$TENANT"
 DATABASE="${TENANT}_domain_${TENANT}"
 CONNECTSTRING="host=$SERVER dbname=$DATABASE"
@@ -29,6 +28,12 @@ time psql -F $'\t' -R"@@" -A -U $USERNAME -d "$CONNECTSTRING" -f media.sql | per
 time psql -F $'\t' -R"@@" -A -U $USERNAME -d "$CONNECTSTRING" -f basic.sql | perl -pe 's/[\r\n]/ /g;s/\@\@/\n/g' > public.csv
 ##############################################################################
 # stitch this together with the results of the rest of the "subqueries"
+# there are 3 query patterns which based on a couple of parameters
+# will pull out multivalued fields, aggregate them, and spit them out with
+# the top level object csid
+#
+# the patterns are in the template*.sql files
+# the parameters are in the type*.txt files
 ##############################################################################
 for var in `cat type1.txt`
 do
@@ -98,7 +103,9 @@ do
   time awk -v NUMCOLS=$NUMCOLS '{ FS = "\t" ; if (NF == 0+NUMCOLS) print }' ${core}.csv | perl -pe 's/\\/\//g;s/\t"/\t/g;s/"\t/\t/g;' > 4solr.$TENANT.base.${core}.csv &
   time awk -v NUMCOLS=$NUMCOLS '{ FS = "\t" ; if (NF != 0+NUMCOLS) print }' ${core}.csv | perl -pe 's/\\/\//g' > errors.${core}.csv &
   wait
+  # merge media and metadata files (done in perl ... very complicated to do in SQL)
   time perl mergeObjectsAndMedia.pl 4solr.$TENANT.media.csv 4solr.$TENANT.base.${core}.csv > d6.csv
+  # recover the solr header and put it back at the top of the file
   grep csid d6.csv > header4Solr.csv
   perl -i -pe 's/$/blob_ss/;' header4Solr.csv
   grep -v csid d6.csv > d8.csv
@@ -116,7 +123,7 @@ do
   # note, among other things, the overriding of the encapsulator with \
   ##############################################################################
   time curl -S -s "http://localhost:8983/solr/${TENANT}-${core}/update/csv?commit=true&header=true&separator=%09&f.assocculturalcontext_ss.split=true&f.assocculturalcontext_ss.separator=%7C&f.assocorganization_ss.split=true&f.assocorganization_ss.separator=%7C&f.assocperson_ss.split=true&f.assocperson_ss.separator=%7C&f.assocplace_ss.split=true&f.assocplace_ss.separator=%7C&f.material_ss.split=true&f.material_ss.separator=%7C&f.measuredpart_ss.split=true&f.measuredpart_ss.separator=%7C&f.objectproductionorganization_ss.split=true&f.objectproductionorganization_ss.separator=%7C&f.objectproductionperson_ss.split=true&f.objectproductionperson_ss.separator=%7C&f.objectproductionplace_ss.split=true&f.objectproductionplace_ss.separator=%7C&f.title_ss.split=true&f.title_ss.separator=%7C&f.loanstatus_ss.split=true&f.loanstatus_ss.separator=%7C&f.lender_ss.split=true&f.lender_ss.separator=%7C&f.comments_ss.split=true&f.comments_ss.separator=%7C&f.styles_ss.split=true&f.styles_ss.separator=%7C&f.colors_ss.split=true&f.colors_ss.separator=%7C&f.contentconcepts_ss.split=true&f.contentconcepts_ss.separator=%7C&f.contentplaces_ss.split=true&f.contentplaces_ss.separator=%7C&f.contentpersons_ss.split=true&f.contentpersons_ss.separator=%7C&f.contentorganizations_ss.split=true&f.contentorganizations_ss.separator=%7C&f.exhibitionhistories_ss.split=true&f.exhibitionhistories_ss.separator=%7C&f.loanoutnumber_ss.split=true&f.loanoutnumber_ss.separator=%7C&f.borrower_ss.split=true&f.borrower_ss.separator=%7C&f.loaninnumber_ss.split=true&f.loaninnumber_ss.separator=%7C&f.blob_ss.split=true&f.blob_ss.separator=,&encapsulator=\\" --data-binary @4solr.$TENANT.${core}.csv -H 'Content-type:text/plain; charset=utf-8'
-  time ./evaluate.sh @4solr.$TENANT.${core}.csv > 4solr.fields.$TENANT.${core}.csv
+  time ./evaluate.sh 4solr.$TENANT.${core}.csv > 4solr.fields.$TENANT.${core}.csv
 done
 ##############################################################################
 # wrap things up: make a gzipped version of what was loaded
